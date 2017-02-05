9b589398d122ca2016c8abff45fa260ed53d9239
Cole Turner <coleski1@gmail.com>
2017-02-04 16:52:16 -0800

    Added jQuery Function
    added the ability for jQuery to hide certain things

--- js.js
+++ js.js
@@ -1,9 +1,23 @@
+
+
 $(document).ready(function() {
 
-  $('.cssinfo').hide();
+  $(".cssinfo").hide();
+  $(".htmlinfo").hide();
+  $(".jsinfo").hide();
+  $(".jqinfo").hide();
 
-  $('.cssinfobutton').click(function() {
-    $ (this).next().slideToggle(300);
+  $(".cssinfobutton").click(function() {
+    $ (".cssinfo").fadeToggle(300);
+  });
+  $(".htmlinfobutton").click(function() {
+    $ (".htmlinfo").fadeToggle(300);
+  });
+  $(".jsinfobutton").click(function() {
+    $ (".jsinfo").fadeToggle(300);
+  });
+  $(".jqinfobutton").click(function() {
+    $ (".jqinfo").fadeToggle(300);
   });
 
 });
